46d766b74a3ad4ccdb0cac4767e89a2cb79c8d15
FraBona <francescobonafede08@gmail.com>
2023-11-21 15:29:18 +0100

    print length replace and new length

--- badwords.php
+++ badwords.php
@@ -1,6 +1,6 @@
 <?php
   $text = 'è un testo segnaposto utilizzato nel settore della tipografia e della stampa. Lorem Ipsum è considerato il testo segnaposto standard sin dal sedicesimo secolo, quando un anonimo tipografo prese una cassetta di caratteri e li assemblò per preparare un testo campione. È sopravvissuto non solo a più di cinque secoli, ma anche al passaggio alla videoimpaginazione, pervenendoci sostanzialmente inalterato. Fu reso popolare, negli anni ’60, con la diffusione dei fogli di caratteri trasferibili “Letraset”, che contenevano passaggi del Lorem Ipsum, e più recentemente da software di impaginazione come Aldus PageMaker, che includeva versioni del Lorem Ipsum.';
-  $word = $_POST['badword']
+  $word = $_POST['badword'];
 ?>
 
 <!DOCTYPE html>
@@ -11,7 +11,9 @@
   <title>Document</title>
 </head>
 <body>
-  <p><?php str_replace($text, $word, "Lorem Ipsum") ?></p>
   <p><?php echo $text?></p>
+  <p><?php echo strlen($text)?></p>
+  <p><?php echo $new_text = str_replace($word, '***', $text) ?></p>
+  <p><?php echo strlen($new_text)?></p>
 </body>
 </html>
