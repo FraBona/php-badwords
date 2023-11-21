c10736ce9074b67c760cdc9edeaf5e6d52197ce6
FraBona <francescobonafede08@gmail.com>
2023-11-21 15:19:34 +0100

    first commit

---
+++ badwords.php
@@ -0,0 +1,17 @@
+<?php
+  $text = 'è un testo segnaposto utilizzato nel settore della tipografia e della stampa. Lorem Ipsum è considerato il testo segnaposto standard sin dal sedicesimo secolo, quando un anonimo tipografo prese una cassetta di caratteri e li assemblò per preparare un testo campione. È sopravvissuto non solo a più di cinque secoli, ma anche al passaggio alla videoimpaginazione, pervenendoci sostanzialmente inalterato. Fu reso popolare, negli anni ’60, con la diffusione dei fogli di caratteri trasferibili “Letraset”, che contenevano passaggi del Lorem Ipsum, e più recentemente da software di impaginazione come Aldus PageMaker, che includeva versioni del Lorem Ipsum.';
+  $word = $_POST['badword']
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <p><?php str_replace($text, $word, "Lorem Ipsum") ?></p>
+  <p><?php echo $text?></p>
+</body>
+</html>
